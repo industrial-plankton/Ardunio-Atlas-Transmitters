4058c0bb0b5dcc8fb367e6742f2ac53a9a70b7bb
Cam Gregory <cameronrkgregory@protonmail.com>
2022-04-27 10:48:19 -0700

    feat: Use and pass local Union's instead of static global

--- Atlas-Transmitter.cpp
+++ Atlas-Transmitter.cpp
@@ -42,19 +42,20 @@ unsigned char CheckI2C()
 
 union sensor_mem_handler // declare the use of a union data type
 {
-    unsigned char i2c_data[4]; // define a 4 byte array in the union
-    long answ;                 // define an long in the union
+    uint8_t i2c_data[4]; // define a 4 byte array in the union
+    uint8_t asByte;
+    uint16_t asInt;
+    uint32_t asLong = 0; // define an long in the union
 };
-static union sensor_mem_handler move_data; // declare that we will refer to the union as move_data
 
 //*************************************************************************************************************************
 //*************************************************************************************************************************
 
 // used to read 1,2,and 4 bytes: i2c_read(starting register,number of bytes to read)
-void i2c_read(const unsigned char reg, const unsigned char number_of_bytes_to_read, const unsigned char bus_address)
+sensor_mem_handler i2c_read(const unsigned char reg, const unsigned char number_of_bytes_to_read, const unsigned char bus_address)
 {
     unsigned char i; // counter
-
+    sensor_mem_handler temp;
     Wire.beginTransmission(bus_address); // call the device by its ID number
     Wire.write(reg);                     // transmit the register that we will start from
     i2cError = Wire.endTransmission();   // end the I2C data transmission
@@ -62,45 +63,62 @@ void i2c_read(const unsigned char reg, const unsigned char number_of_bytes_to_re
     Wire.requestFrom(bus_address, number_of_bytes_to_read); // call the device and request to read X bytes
     for (i = number_of_bytes_to_read; i > 0; i--)
     {
-        move_data.i2c_data[i - 1] = Wire.read();
+        temp.i2c_data[i - 1] = Wire.read();
     }                                  // with this code we read multiple bytes in reverse
     i2cError = Wire.endTransmission(); // end the I2C data transmission
+    return temp;
 }
 
+// Read Single Byte
+int i2c_read(const unsigned char reg, const unsigned char bus_address)
+{
+    Wire.beginTransmission(bus_address); // call the device by its ID number
+    Wire.write(reg);                     // transmit the register that we will start from
+    i2cError = Wire.endTransmission();   // end the I2C data transmission
+
+    Wire.requestFrom(bus_address, 1); // call the device and request to read X bytes
+    const int value = Wire.read();
+    // with this code we read multiple bytes in reverse
+    Wire.endTransmission(); // end the I2C data transmission
+    return value;
+}
 //*************************************************************************************************************************
 //*************************************************************************************************************************
 
-void i2c_write_byte(const unsigned char reg, const unsigned char data, const unsigned char bus_address)
+uint8_t i2c_write_byte(const unsigned char reg, const unsigned char data, const unsigned char bus_address)
 {                                        // used to write a single byte to a register: i2c_write_byte(register to write to, byte data)
     Wire.beginTransmission(bus_address); // call the device by its ID number
     Wire.write(reg);                     // transmit the register that we will start from
     Wire.write(data);                    // write the byte to be written to the register
     i2cError = Wire.endTransmission();   // end the I2C data transmission
+    return i2cError;
 }
 
 //*************************************************************************************************************************
 //*************************************************************************************************************************
 
 // used to write a 4 bytes to a register: i2c_write_long(register to start at, long data )
-void i2c_write_long(const unsigned char reg, const unsigned long data, const unsigned char bus_address)
+uint8_t i2c_write_long(const unsigned char reg, const unsigned long data, const unsigned char bus_address)
 {
     unsigned char i; // counter
-    move_data.answ = data;
+    sensor_mem_handler temp;
+    temp.asLong = data;
 
     Wire.beginTransmission(bus_address); // call the device by its ID number
     Wire.write(reg);                     // transmit the register that we will start from
     for (i = 4; i > 0; i--)
     { // with this code we write multiple bytes in reverse
-        Wire.write(move_data.i2c_data[i - 1]);
+        Wire.write(temp.i2c_data[i - 1]);
     }
     i2cError = Wire.endTransmission(); // end the I2C data transmission
+    return i2cError;
 }
 
 //*************************************************************************************************************************
 //*************************************************************************************************************************
 
 // calibration multiplyied by 1,000 , type: 1 = temperature, 2 = low , 3 = mid, 4 = high
-bool calibration(const unsigned long calibration, const unsigned char type)
+bool calibration(const unsigned long calibration, const unsigned int type)
 {
     const unsigned char calibration_value_register = 0x08;        // register to read / write
     const unsigned char calibration_request_register = 0x0C;      // register to read / write
@@ -117,29 +135,27 @@ bool calibration(const unsigned long calibration, const unsigned char type)
         i2c_write_long(calibration_value_register, calibration, i2c_id_temp);            // write the 4 bytes of the long to the calibration register
         i2c_write_byte(calibration_request_register, temperaturecalibrate, i2c_id_temp); // write the calibration command to the calibration control register
         delay(10);                                                                       // wait for the calibration event to finish
-        i2c_read(calibration_confirmation_register, one_byte_read, i2c_id_temp);         // read from the calibration control register to confirm it is set correctly
+        return i2c_read(calibration_confirmation_register, i2c_id_temp);                 // read from the calibration control register to confirm it is set correctly
     }
     else if (type == 5)
     {
         i2c_write_byte(calibration_request_register, cal_clear, i2c_id_ph); // write the calibration clear command to the calibration control register
         delay(10);                                                          // wait for the calibration event to finish
-        i2c_read(calibration_confirmation_register, one_byte_read, i2c_id_ph);
+        return i2c_read(calibration_confirmation_register, i2c_id_ph);
     }
     else if (type == 6)
     {
         i2c_write_byte(calibration_request_register, cal_clear, i2c_id_temp); // write the calibration clear command to the calibration control register
         delay(10);                                                            // wait for the calibration event to finish
-        i2c_read(calibration_confirmation_register, one_byte_read, i2c_id_temp);
+        return i2c_read(calibration_confirmation_register, i2c_id_temp);
     }
     else
     {
-        i2c_write_long(calibration_value_register, calibration, i2c_id_ph);    // write the 4 bytes of the unsigned long to the calibration register
-        i2c_write_byte(calibration_request_register, type, i2c_id_ph);         // write the calibration command to the calibration control register
-        delay(165);                                                            // wait for the calibration event to finish
-        i2c_read(calibration_confirmation_register, one_byte_read, i2c_id_ph); // read from the calibration control register to confirm it is set correctly
+        i2c_write_long(calibration_value_register, calibration, i2c_id_ph); // write the 4 bytes of the unsigned long to the calibration register
+        i2c_write_byte(calibration_request_register, type, i2c_id_ph);      // write the calibration command to the calibration control register
+        delay(165);                                                         // wait for the calibration event to finish
+        return i2c_read(calibration_confirmation_register, i2c_id_ph);      // read from the calibration control register to confirm it is set correctly
     }
-
-    return true; // move_data.i2c_data[0];
 }
 
 void temp_comp(const unsigned long compensation)
@@ -157,13 +173,18 @@ bool efficientConfig(const unsigned char bus_address)
     Wire.beginTransmission(bus_address); // call the device by its ID number
     Wire.write(0x04);                    // transmit the register that we will start from
     Wire.write(0x00);                    // Set INT Pin Mode (0 disable, 2 high on new, 4 low on new, 8 invert on new)
-    Wire.write(0x01);                    //Set LED Mode (1 blink, 0 off)
+    Wire.write(0x01);                    // Set LED Mode (1 blink, 0 off)
     Wire.write(0x01);                    // Set Hib Mode (1 read, 0 sleep)
     i2cError = Wire.endTransmission();   // end the I2C data transmission
 
-    i2c_read(0x06, one_byte_read, bus_address); // read from the active / hibernate control register to confirm it is set correctly
-
-    return move_data.i2c_data[0];
+    if (bus_address != 0)
+    {
+        return i2c_read(0x06, one_byte_read, bus_address).asByte; // read from the active / hibernate control register to confirm it is set correctly
+    }
+    else
+    {
+        return true;
+    }
 }
 
 //*************************************************************************************************************************
@@ -175,12 +196,12 @@ long Temp_reading()
     const unsigned char RTD_register = 0x0E; // register to read
     // float RTD = 0;													//used to hold the new RTD value
 
-    i2c_read(RTD_register, four_byte_read, i2c_id_temp); // I2C_read(OEM register, number of bytes to read)
-    const long temp = move_data.answ;
-    return temp; // move_data.answ;                               //move the 4 bytes read into a float
-                 //  RTD /= 1000;														//divide by 1000 to get the decimal point
-                 //  Serial.print("RTD= ");
-                 //  Serial.println(RTD, 3);	                                        //print info from register block
+    return i2c_read(RTD_register, four_byte_read, i2c_id_temp).asLong; // I2C_read(OEM register, number of bytes to read)
+    // const long temp = move_data.asLong;
+    // return temp; // move_data.answ;                               //move the 4 bytes read into a float
+    //  RTD /= 1000;														//divide by 1000 to get the decimal point
+    //  Serial.print("RTD= ");
+    //  Serial.println(RTD, 3);	                                        //print info from register block
 }
 
 //  pH /= 1000;
@@ -189,11 +210,11 @@ long pH_reading()
     const unsigned char pH_register = 0x16; // register to read
     // float pH = 0;													//used to hold the new pH value
 
-    i2c_read(pH_register, four_byte_read, i2c_id_ph); // I2C_read(OEM register, number of bytes to read)
-    return move_data.answ;                            // move the 4 bytes read into a float
-                                                      //  pH /= 1000;														//divide by 1000 to get the decimal point
-                                                      //  Serial.print("pH= ");
-                                                      //  Serial.println(pH);	                                        //print info from register block
+    return i2c_read(pH_register, four_byte_read, i2c_id_ph).asLong; // I2C_read(OEM register, number of bytes to read)
+    // return move_data.answ;                                   // move the 4 bytes read into a float
+    //  pH /= 1000;														//divide by 1000 to get the decimal point
+    //  Serial.print("pH= ");
+    //  Serial.println(pH);	                                        //print info from register block
 }
 
 void Atlas::Initialize() const
@@ -218,8 +239,8 @@ long Atlas::Read() const
         return -1;
     }
 
-    i2c_read(readReg, four_byte_read, i2cAddress); // I2C_read(OEM register, number of bytes to read)
-    return move_data.answ;                         // move the 4 bytes read into a long
+    return i2c_read(readReg, four_byte_read, i2cAddress).asLong; // I2C_read(OEM register, number of bytes to read)
+    // return move_data.answ;                                       // move the 4 bytes read into a long
 }
 
 void Atlas::Calibrate(const unsigned long calibrationValue, const CalibrationType type) const
